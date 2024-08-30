130c726f29c646b218fe72caa7f122c1205b2fb9
Gustavo Sant'Anna <g_santanna@outlook.com>
2024-08-30 19:47:23 -0300

    feat: added measures service

--- src/models/measuresModel.ts
+++ src/models/measuresModel.ts
@@ -1,6 +1,6 @@
 import {
-  Measure,
   MeasureType,
+  Prisma,
   PrismaClient as PrismaClientType,
 } from '@prisma/client';
 import PrismaClient from './prismaClient';
@@ -12,22 +12,44 @@ class MeasuresModel {
     this.prisma = PrismaClient;
   }
 
-  async create(data: Measure) {
+  async create(data: Prisma.MeasureCreateInput) {
     return this.prisma.measure.create({ data });
   }
 
-  async find(customer_code: string, measure_type?: MeasureType) {
-    return this.prisma.measure.findMany({
-      where: {
-        ...(measure_type ? { measure_type } : {}),
-        customer: {
-          customer_code,
-        },
-      },
+  async findByUuid(measure_uuid: string) {
+    return this.prisma.measure.findFirst({
+      where: { measure_uuid },
     });
   }
 
-  async update(measure_uuid: string, data: Partial<Measure>) {
+  async findByCustomer(
+    customer_code: string,
+    params: {
+      measure_type?: MeasureType;
+      get_last?: boolean;
+    } = {},
+  ) {
+    const { measure_type, get_last } = params;
+    const where: { [key: string]: any } = {
+      customer: {
+        customer_code,
+      },
+    };
+
+    if (measure_type) {
+      where.measure_type = measure_type;
+    }
+
+    if (get_last) {
+      where.measure_datetime = {
+        gte: new Date(new Date().getFullYear(), new Date().getMonth(), 1),
+      };
+    }
+
+    return this.prisma.measure.findMany({ where });
+  }
+
+  async update(measure_uuid: string, data: Prisma.MeasureUpdateInput) {
     return this.prisma.measure.update({
       where: { measure_uuid },
       data,
---
+++ src/services/measureService.ts
@@ -0,0 +1,111 @@
+// src/services/measureService.ts
+import { MeasureType, Prisma } from '@prisma/client';
+import geminiService from './geminiService';
+import measuresModel from '../models/measuresModel';
+import customersModel from '../models/customersModel';
+
+interface CreateMeasurePayload {
+  customer_code: string;
+  measure_datetime: Date;
+  measure_type: MeasureType;
+  image: string;
+}
+
+class MeasureService {
+  async createMeasure(data: CreateMeasurePayload) {
+    const { measure_type, customer_code, image, measure_datetime } = data;
+    const existingMeasure = await measuresModel.findByCustomer(
+      data.customer_code,
+      {
+        measure_type,
+        get_last: true,
+      },
+    );
+
+    if (existingMeasure) {
+      return {
+        status_code: 409,
+        error_code: 'DOUBLE_REPORT',
+        error_description: 'Leitura do mês já realizada',
+      };
+    }
+
+    // Obter o valor da medida usando o GeminiService
+    const { measure_value, image_url } =
+      await geminiService.processImage(image);
+
+    const customer = await customersModel.find(customer_code);
+
+    const payload = {
+      measure_datetime,
+      measure_type,
+      measure_value,
+      image_url,
+      has_confirmed: false,
+    } as Prisma.MeasureCreateInput;
+
+    payload.customer = customer
+      ? { connect: { id: customer.id } }
+      : { create: { customer_code } };
+
+    const newMeasure = await measuresModel.create(payload);
+
+    return {
+      status_code: 200,
+      data: newMeasure,
+    };
+  }
+
+  async confirmMeasure(measure_uuid: string, confirmed_value: number) {
+    const measure = await measuresModel.findByUuid(measure_uuid);
+
+    if (!measure) {
+      return {
+        status_code: 404,
+        error_code: 'MEASURE_NOT_FOUND',
+        // error_description não faz sentido, pois a leitura não foi encontrada mas a doc do desafio pede tal valor
+        error_description: 'Leitura do mês já realizada',
+      };
+    }
+
+    if (measure.has_confirmed) {
+      return {
+        status_code: 409,
+        error_code: 'CONFIRMATION_DUPLICATE',
+        // error_description não faz sentido, pois a leitura já foi confirmada mas a doc do desafio pede tal valor
+        error_description: 'Leitura do mês já realizada',
+      };
+    }
+
+    const updatedMeasure = await measuresModel.update(measure_uuid, {
+      measure_value: confirmed_value,
+      has_confirmed: true,
+    });
+
+    return updatedMeasure;
+  }
+
+  async listMeasures(customer_code: string, measure_type?: MeasureType) {
+    const measures = await measuresModel.findByCustomer(customer_code, {
+      measure_type,
+    });
+
+    if (!measures.length) {
+      return {
+        status_code: 404,
+        error_code: 'MEASURES_NOT_FOUND',
+        error_description: 'Nenhuma leitura encontrada',
+      };
+    }
+
+    return {
+      status_code: 200,
+      data: {
+        customer_code,
+        measures,
+      },
+    };
+  }
+}
+
+export default new MeasureService();
